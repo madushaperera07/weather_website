9eecc6590f8929239b9defbc8b6f94216c8a0af8
madushaperera07 <madushanilakshan9@gmail.com>
2023-07-26 14:05:48 +0530

    create frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+
+    <section className="App">
+      <div className="left_containe">
+          <div className="weather_header">
+            <div className="header_containe">
+              <h5>Weather</h5>
+            </div>    
+            <div className="location_containe">
+              
+              <i class="bi bi-geo-alt-fill loc_icon"></i>
+              <div>
+
+              <p>Current Location</p>
+
+              <p>country , city</p>
+              </div>
+            </div>
+          </div>
+      
+        
+        <form className="input_containe">
+          <div>
+          <h6>The Only Weather Forecast You Need</h6>
+          <div className="input_one">
+            <div>
+            <div className="line_containe"></div>
+            <div className="input_location">
+            
+              <input type="text" class="form-control input_all" placeholder="Enter Location"/>
+              <button type="submit" class="btn btn-outline-light"><i class="bi bi-search"></i></button>
+            
+            </div>
+
+                       
+            </div>
+          </div>
+
+          </div>
+        </form>
+      </div>
+      
+          
+      <div className="right_containe">
+      <h2>Today</h2>
+      <div className="weater_containe">
+
+      </div>
+      </div>
+    </section>
+  </>
   );
 }
 
 export default App;
+
