42bed9bad5d7cbe28e202093821de9182c613d36
madushaperera07 <madushanilakshan9@gmail.com>
2023-07-28 23:24:48 +0530

    update client side

--- src/App.js
+++ src/App.js
@@ -1,61 +1,71 @@
-
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  return (
-  <>
+  const [val, setVal] = useState("");
 
-    <section className="App">
-      <div className="left_containe">
-          <div className="weather_header">
-            <div className="header_containe">
+  async function submit(e) {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://localhost:5000/", {
+        val,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  return (
+    <>
+      <section class="App">
+        <div class="left_containe">
+          <div class="weather_header">
+            <div class="header_containe">
               <h5>Weather</h5>
-            </div>    
-            <div className="location_containe">
-              
+            </div>
+            <div class="location_containe">
               <i class="bi bi-geo-alt-fill loc_icon"></i>
               <div>
+                <p>Current Location</p>
 
-              <p>Current Location</p>
-
-              <p>country , city</p>
+                <p>country , city</p>
               </div>
             </div>
           </div>
-      
-        
-        <form className="input_containe">
-          <div>
-          <h6>The Only Weather Forecast You Need</h6>
-          <div className="input_one">
+
+          <div class="input_containe">
             <div>
-            <div className="line_containe"></div>
-            <div className="input_location">
-            
-              <input type="text" class="form-control input_all" placeholder="Enter Location"/>
-              <button type="submit" class="btn btn-outline-light"><i class="bi bi-search"></i></button>
-            
-            </div>
+              <h6>The Only Weather Forecast You Need</h6>
+              <div class="input_one">
+                <div>
+                  <div class="line_containe"></div>
 
-                       
+                  <div class="input_location">
+                    <form action="POST">
+                      <input
+                        name="text"
+                        onChange={(e) => {
+                          setVal(e.target.value);
+                        }}
+                        placeholder="Enter Location"
+                      />
+                      <input type="submit" onClick={submit} class="button" />
+                    </form>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
+        </div>
 
-          </div>
-        </form>
-      </div>
-      
-          
-      <div className="right_containe">
-      <h2>Today</h2>
-      <div className="weater_containe">
-
-      </div>
-      </div>
-    </section>
-  </>
+        <div class="right_containe">
+          <h2>Today</h2>
+          <div class="weater_containe"></div>
+        </div>
+      </section>
+    </>
   );
 }
 
 export default App;
-
